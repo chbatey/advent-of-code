def1f0b5ece5e01b72bb9be2217145fe78f3f666
Christopher Batey <christopher.batey@gmail.com>
2016-12-20 20:14:12 +0000

    Initial commit

---
+++ AdventOfCode/BathroomCode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Cli
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there");
+        }
+    }
+
+    public class BathroomCode
+    {
+        private string code = "";
+        private char[][] board;
+        private int xLocation = 0;
+        private int yLocation = 2;
+
+        public BathroomCode()
+        {
+            board = new[]
+            {
+                new char[] {'-', '-', '1', '-', '-'},
+                new char[] {'-', '2', '3', '4', '-'},
+                new char[] {'5', '6', '7', '8', '9'},
+                new char[] {'-', 'A', 'B', 'C', '-'},
+                new char[] {'-', '-', 'D', '-', '-'},
+            };
+        }
+
+        public void AddCode(string moves)
+        {
+            foreach (char move in moves.ToCharArray())
+            {
+                switch (move)
+                {
+                    case 'R':
+                        xLocation++;
+                        break;
+                    case 'L':
+                        break;
+                    case 'D':
+                        if (board[yLocation + 1][xLocation] != '-')
+                        {
+                            yLocation++;
+                        }
+                        break;
+                    case 'U':
+                        if (board[yLocation - 1][xLocation] != '-')
+                        {
+                            yLocation--;
+                        }
+                        break;
+                    default:
+                        throw new NotImplementedException();
+                }
+            }
+            code += board[yLocation][xLocation];
+        }
+
+        public string Code()
+        {
+            return code;
+        }
+    }
+}
---
+++ AdventOfCodeTest/BathroomCodeTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+using AdventOfCode;
+
+namespace AdventOfCodeTest
+{
+    public class BathroomCodeTest
+    {
+        private BathroomCode underTest;
+
+        public BathroomCodeTest()
+        {
+            underTest = new BathroomCode();
+        }
+
+        [Fact]
+        public void AtStart()
+        {
+            Assert.Equal("", underTest.Code());
+        }
+
+        [Fact]
+        public void UpAtStart()
+        {
+            underTest.AddCode("U");
+            Assert.Equal("5", underTest.Code());
+        }
+
+        [Fact]
+        public void DownAtStart()
+        {
+            underTest.AddCode("D");
+            Assert.Equal("5", underTest.Code());
+        }
+
+        [Fact]
+        public void LeftAtStart()
+        {
+            underTest.AddCode("L");
+            Assert.Equal("5", underTest.Code());
+        }
+
+        [Fact]
+        public void RightAtStart()
+        {
+            underTest.AddCode("R");
+            Assert.Equal("6", underTest.Code());
+        }
+
+        [Fact]
+        public void Down()
+        {
+            underTest.AddCode("RD");
+            Assert.Equal("A", underTest.Code());
+        }
+
+        [Fact]
+        public void Up()
+        {
+            underTest.AddCode("RU");
+            Assert.Equal("2", underTest.Code());
+        }
+    }
+}
